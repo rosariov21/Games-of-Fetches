976d64a178d8c58610653ae94de9133174f512f7
rosariov21 <rosariovelazquez47@yahoo.com>
2020-08-04 11:51:05 -0400

    third commit

--- src/App.js
+++ src/App.js
@@ -1,18 +1,9 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import axios from 'axios';
 
 
-componentDidMount(){
-  axios.get(' http://anapioficeandfire.com/api/characters/583')
-  .then(response =>{
-    console.log(response.data);
-  })
-  .catch(error =>{
-    console.log(error);
-  });
-}
+
 
 
 function App() {
--- src/index.js
+++ src/index.js
@@ -1,20 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import axios from "axios";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+let one =
+  "https://api.storyblok.com/v1/cdn/stories/health?version=published&token=wANpEQEsMYGOwLxwXQ76Ggtt";
+let two =
+  "https://api.storyblok.com/v1/cdn/datasources/?token=wANpEQEsMYGOwLxwXQ76Ggtt";
+let three =
+  "https://api.storyblok.com/v1/cdn/stories/vue?version=published&token=wANpEQEsMYGOwLxwXQ76Ggtt";
 
+const requestOne = axios.get(one);
+const requestTwo = axios.get(two);
+const requestThree = axios.get(three);
 
+axios
+  .all([requestOne, requestTwo, requestThree])
+  .then(
+    axios.spread((...responses) => {
+      const responseOne = responses[0];
+      const responseTwo = responses[1];
+      const responesThree = responses[2];
 
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+      // use/access the results
+      console.log(responseOne, responseTwo, responesThree);
+    })
+  )
+  .catch(errors => {
+    // react on errors.
+    console.error(errors);
+  });
