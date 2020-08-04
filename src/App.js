25a1c259a5a517ba5d8972151e793b96e3253b1b
rosariov21 <rosariovelazquez47@yahoo.com>
2020-08-04 11:37:55 -0400

    second commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+
+
+componentDidMount(){
+  axios.get(' http://anapioficeandfire.com/api/characters/583')
+  .then(response =>{
+    console.log(response.data);
+  })
+  .catch(error =>{
+    console.log(error);
+  });
+}
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   
     </div>
   );
 }
